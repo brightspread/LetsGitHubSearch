afa0c30fab20a682f78dd7e4ba7c2c2d023dd792
Leo <brightspread.jo@gmail.com>
2024-01-13 18:58:08 +0900

    ♻️ Reducer 모델링

--- starter/GitHubSearch/Repository/RepoSearch.swift
+++ starter/GitHubSearch/Repository/RepoSearch.swift
@@ -2,22 +2,41 @@ import Foundation
 
 import ComposableArchitecture
 
+// Reducer 모델링
 struct RepoSearch: ReducerProtocol {
-  struct State: Equatable {
-    // TODO: 지금 앱은 어떤 상태들로 정의되는가?
-    // keyword와 searchResults 상태 추가하기
-  }
 
-  enum Action: Equatable {
-    // TODO: 상태들을 변화시키는 사용자의 액션은 무엇인가?
-    // keywordChanged, search 액션 추가하기
-  }
+    private let sampleRepoLists = [
+      "Swift",
+      "SwiftyJSON",
+      "SwiftGuide",
+      "SwiftterSwift",
+    ]
 
-  func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
-    // TODO: 각각의 Action이 발생했을 때 상태는 어떻게 변화해야 하는가?
-//    switch action {
-//    case let .keywordChanged(keyword):
-//    case .search:
-//    }
-  }
+    struct State: Equatable {
+        // TODO: 지금 앱은 어떤 상태들로 정의되는가?
+        // keyword와 searchResults 상태 추가하기
+        var keyword = ""
+        var searchResults = [String]()
+    }
+
+    enum Action: Equatable {
+        // TODO: 상태들을 변화시키는 사용자의 액션은 무엇인가?
+        // keywordChanged, search 액션 추가하기
+        case keywordChanged(String)
+        case search
+    }
+
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        // TODO: 각각의 Action이 발생했을 때 상태는 어떻게 변화해야 하는가?
+        switch action {
+            case let .keywordChanged(keyword):
+                state.keyword = keyword
+                return .none // side effect 없음
+            case .search:
+                state.searchResults = self.sampleRepoLists.filter {
+                  $0.contains(state.keyword)
+                }
+                return .none // side effect 없음
+        }
+    }
 }
